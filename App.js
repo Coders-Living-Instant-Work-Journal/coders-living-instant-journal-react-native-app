675ccac17bb97fe37b6ad0dc681a4e3b7ee6acb2
Susanna Lakey <susannalakey@gmail.com>
2020-04-12 19:25:29 -0700

    added header and footer

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Container } from "native-base";
 
-export default function App() {
+import MainHeader from "./src/components/MainHeader";
+import MainFooter from "./src/components/MainFooter";
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-    </View>
+    <>
+      <Container>
+        <MainHeader />
+        <MainFooter />
+      </Container>
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
